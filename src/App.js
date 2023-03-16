66bbd18dbe01e225f06e30e4ec8cef35896d8264
Sizimon <siizmonsus@gmail.com>
2023-03-16 20:44:05 +0000

    Setup react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Container } from 'react-bootstrap';
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import {Switch, Route} from 'react-router-dom';
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className={styles.App} >
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() =><h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() =><h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() =><h1>Sign Up</h1>} />
+          <Route render={() => <p>Page not found.</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
